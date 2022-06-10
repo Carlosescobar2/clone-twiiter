41521d306e9dc77d5d20c1bce74b2cff28dd4fdd
Carlos Escobar <escobarcarlos@gmail.com>
2022-06-09 17:52:18 -0700

    tweetbox-grid

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -10,17 +10,17 @@ import {
 
 const TweetBox = () => {
   return (
-    <div>
+    <div className="flex space-x-2 p-5">
       <img
         className="h-14 w-14 rounded-full object-cover mt-4"
         src="https://links.papareact.com/gll"
         alt=""
       />
-      <div>
-          <form action="">
-              <input type="text" placeholder="What's Happening?" />
-              <div>
-                  <div className="flex space-x-2 text-twitter">
+      <div className="flex flex-1 items-center pl-2">
+          <form  className="flex flex-1 flex-col" action="">
+              <input type="text" placeholder="What's Happening?" className="h-24 w-full text-xl outline-none placeholder:text-xl" />
+              <div className="flex items-center">
+                  <div className="flex flex-1 space-x-2 text-twitter">
                     <PhotographIcon className="h-5 w-5" />
                     <SearchCircleIcon className="h-5 w-5" /> 
                     <EmojiHappyIcon className="h-5 w-5" /> 
@@ -28,7 +28,7 @@ const TweetBox = () => {
                     <LocationMarkerIcon className="h-5 w-5" /> 
 
                   </div>
-                  <button className="bg-twitter">Tweet</button>
+                  <button className="bg-twitter px-5 py-2 font-bold text-white rounded-full">Tweet</button>
               </div>
           </form>
       </div>
